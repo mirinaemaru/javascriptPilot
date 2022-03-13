a0cadf3726336e6cd67b4319242011f17c382aed
mirinae.maru <mirinae.maru@gmail.com>
2022-03-14 08:24:46 +0900

    agGrid cell style 공부 정리 중

--- js/array_sort.js
+++ js/array_sort.js
@@ -1,25 +1,26 @@
 'use strict';
 
 let sortArray = [
-    {name:'mirinae', age:23},
-    {name:'mirinae', age:21},
-    {name:'mirinae', age:2},
-    {name:'mirinae', age:34},
-    {name:'mirinae', age:55},
+    {name:'mirinae1', age:23},
+    {name:'mirinae3', age:21},
+    {name:'mirinae7', age:2},
+    {name:'mirinae4', age:34},
+    {name:'mirinae2', age:55},
 ];
 
 
 console.log('>>> before');
 console.log(sortArray);
 
+
 sortArray.sort((a,b)=>{
-    if(a.age>b.age) {
+    if(a.name>b.name) {
         return 1;
     }
-    if(a.age<b.age) {
+    if(a.name<b.name) {
         return -1;
     }
-    return 0
+    return 0;
 });
 
 
